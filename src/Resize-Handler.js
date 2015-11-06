b58d98a6f5d824747cee0ee1fd724bff90b35b0c
Travis Arnold <ftntravis@gmail.com>
2015-11-06 15:25:08 -0800

    added requestAnimationFrame to resize handler

--- src/Resize-Handler.js
+++ src/Resize-Handler.js
@@ -1,17 +1,17 @@
 class ResizeHandler {
   constructor() {
     this._queue = []
-    this.update = this.update.bind(this)
+    this._isTicking = false
     this.create()
   }
 
   create() {
-    window.addEventListener('resize', this.update)
+    window.addEventListener('resize', this._resizeHandler)
   }
 
   destroy() {
     this._queue = []
-    window.removeEventListener('resize', this.update)
+    window.removeEventListener('resize', this._resizeHandler)
   }
 
   add(component) {
@@ -25,10 +25,18 @@ class ResizeHandler {
     }    
   }
 
-  update() {
+  _resizeHandler = () => {
+    if(!this.isTicking) {
+      window.requestAnimationFrame(this.update)
+    }
+    this.isTicking = true
+  }
+
+  update = () => {
     for (let i = this._queue.length; i--;) {
       this._queue[i].position()
     }
+    this.isTicking = false
   }
 }
 
